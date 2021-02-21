fe37ffc43b2d543db81cb096b1c96bde1f82f4ec
dicorndl <dicorndl@gmail.com>
2021-02-21 20:59:09 +0900

    12.4.2 : 두번째 테스트 2
    - 테스트가 통과하도록 구현.

--- src/main/java/auctionsniper/AuctionMessageTranslator.java
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -1,5 +1,9 @@
 package auctionsniper;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
@@ -14,6 +18,21 @@ public class AuctionMessageTranslator implements MessageListener {
 
   @Override
   public void processMessage(Chat chat, Message message) {
-    listener.auctionClosed();
+    Map<String, String> event = unpackEventFrom(message);
+
+    String type = event.get("Event");
+    if ("CLOSE".equals(type)) {
+      listener.auctionClosed();
+    } else if ("PRICE".equals(type)) {
+      listener.currentPrice(
+          Integer.parseInt(event.get("CurrentPrice")),
+          Integer.parseInt(event.get("Increment")));
+    }
+  }
+
+  private Map<String, String> unpackEventFrom(Message message) {
+    return Arrays.stream(message.getBody().split(";"))
+        .map(element -> element.split(":"))
+        .collect(Collectors.toMap(pair -> pair[0].trim(), pair -> pair[1].trim()));
   }
 }
