c4c7278915947da3071571859d8f1dd196547b3b
신지영 <jiyoung917@zuminternet.com>
2021-02-28 02:29:04 +0900

    13.3.3 번역기 정리

--- src/main/java/auctionsniper/AuctionMessageTranslator.java
+++ src/main/java/auctionsniper/AuctionMessageTranslator.java
@@ -1,6 +1,7 @@
 package auctionsniper;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -18,21 +19,56 @@ public class AuctionMessageTranslator implements MessageListener {
 
   @Override
   public void processMessage(Chat chat, Message message) {
-    Map<String, String> event = unpackEventFrom(message);
+    AuctionEvent event = AuctionEvent.from(message.getBody());
 
-    String type = event.get("Event");
+    String type = event.type();
     if ("CLOSE".equals(type)) {
       listener.auctionClosed();
     } else if ("PRICE".equals(type)) {
-      listener.currentPrice(
-          Integer.parseInt(event.get("CurrentPrice")),
-          Integer.parseInt(event.get("Increment")));
+      listener.currentPrice(event.currentPrice(), event.increment());
     }
   }
 
-  private Map<String, String> unpackEventFrom(Message message) {
-    return Arrays.stream(message.getBody().split(";"))
-        .map(element -> element.split(":"))
-        .collect(Collectors.toMap(pair -> pair[0].trim(), pair -> pair[1].trim()));
+  private static class AuctionEvent {
+
+    private final Map<String, String> fields = new HashMap<>();
+
+    public String type() {
+      return get("Event");
+    }
+
+    public int currentPrice() {
+      return getInt("CurrentPrice");
+    }
+
+    public int increment() {
+      return getInt("Increment");
+    }
+
+    private int getInt(String fieldName) {
+      return Integer.parseInt(get(fieldName));
+    }
+
+    private String get(String fieldName) {
+      return fields.get(fieldName);
+    }
+
+    private void addField(String field) {
+      String[] pair = field.split(":");
+      fields.put(pair[0].trim(), pair[1].trim());
+    }
+
+    static AuctionEvent from(String messageBody) {
+      AuctionEvent event = new AuctionEvent();
+
+      for (String field : fieldsIn(messageBody)) {
+        event.addField(field);
+      }
+      return event;
+    }
+
+    static String[] fieldsIn(String messageBody) {
+      return messageBody.split(";");
+    }
   }
 }
